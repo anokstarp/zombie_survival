8aaad8d4a97c1c620a61bd21e1f6072b5e27af55
김민지 <minji4885@gmail.com>
2023-07-07 17:42:43 +0900

    타이틀씬 구현중

--- zombie/Scenes/SceneTitle.cpp
+++ zombie/Scenes/SceneTitle.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "SceneTitle.h"
 #include "ResourceMgr.h"
+#include "TextGo.h"
 
 SceneTitle::SceneTitle(SceneId id)
 {
@@ -16,25 +17,38 @@ void SceneTitle::Init()
 {
 	Release();
 
-	//AddGo(new TextGo(""))
+	AddGo(new TextGo("Title", "fonts/zombiecontrol.ttf"));
+
+	for (auto go : gameObjects)
+	{
+		go->Init();
+	}
 }
 
 void SceneTitle::Release()
 {
+	for (auto go : gameObjects)
+	{
+		delete go;
+	}
 }
 
 void SceneTitle::Enter()
 {
+	Scene::Enter();
 }
 
 void SceneTitle::Exit()
 {
+	Scene::Exit();
 }
 
 void SceneTitle::Update(float dt)
 {
+	Scene::Update(dt);
 }
 
 void SceneTitle::Draw(sf::RenderWindow& window)
 {
+	Scene::Draw(window);
 }
