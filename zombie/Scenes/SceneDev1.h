f04a21bb99b0e77267f1d22072dfd3e634920b8b
이남석 <2namsuk2@gmail.com>
2023-07-07 14:54:12 +0900

    초기 커밋

---
+++ zombie/Scenes/SceneDev1.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Scene.h"
+#include "ObjectPool.h"
+
+class Player;
+class Zombie;
+class VertexArrayGo;
+class SpriteEffect;
+
+class SceneDev1 : public Scene
+{
+protected:
+
+	Player* player;
+	ObjectPool<Zombie> zombiePool;
+	ObjectPool<SpriteEffect> bloodEffectPool;
+
+	sf::FloatRect wallBounds;
+	bool isGameOver;
+
+public:
+	SceneDev1();
+	virtual ~SceneDev1() override;
+
+	virtual void Init() override;
+	virtual void Release() override;
+
+	virtual void Enter() override;
+	virtual void Exit() override;
+
+	virtual void Update(float dt) override;
+	virtual void Draw(sf::RenderWindow& window) override;
+
+	VertexArrayGo* CreateBackground(sf::Vector2i size, sf::Vector2f tileSize, sf::Vector2f texSize, std::string textureId);
+
+	void SpawnZombies(int count, sf::Vector2f center, float radius);
+
+	template <typename T>
+	void ClearObjectPool(ObjectPool<T>& pool);
+
+	void ClearZombies();
+
+	void OnDieZombie(Zombie* zombie);
+	void OnDiePlayer();
+
+	const std::list<Zombie*>* GetZombieList() const;
+
+};
+
+template<typename T>
+inline void SceneDev1::ClearObjectPool(ObjectPool<T>& pool)
+{
+	for (auto obj : pool.GetUseList())
+	{
+		RemoveGo(obj);
+	}
+	pool.Clear();
+}
